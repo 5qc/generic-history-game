a42d123eebaa1cd514d6e8dc65491fe4b1477105
ronin <roc0ast3r@gmail.com>
2024-03-28 03:02:47 -0700

    update dist

--- dist/script.js
+++ dist/script.js
@@ -34,26 +34,54 @@ const mediumQuestions = [
     ["who succeeded adolf hitler as leader of germany?", "karl dönitz", "joseph goebbels", "lutz graf schwerin von krosigk", "hans lammers", "otto meissner", "martin bormann", "philip bouhler", "hermann göring", "konstantin von neurath"],
     ["what city did anne frank hide in from the nazis?", "amsterdam", "brussels", "antwerp", "rotterdam", "the hague", "düsseldorf", "strasbourg", "paris", "luxembourg", "london", "berlin"]
 ];
+const hyperspecificQuestions = [
+    ["which candidate received the least amount of votes in the 1964 illinois house of representatives election?", "hellmut w. stolle", "alfred b. two", "norbert l. lundberg", "raymond j. kahoun", "j. theodore meyer", "romie j. palmer", "james d. heiple", "richard l. lodestro", "david w. johnson", "jack d. songer", "eugene t. devitt", "sydney l. perkins", "edward a. bundy", "jack e. walker", "paul k. zeman", "oscar hansen", "john f. wall", "allan l. schoeberlein", "carl t. hunsicker", "ben c. blades"],
+    ["in the last german election before the nazis took over, exactly how many seats did the nazi party win?", "288", "120", "81", "73", "52", "19", "430", "100", "77", "301", "401", "62", "370", "219", "121", "177", "352", "125", "58", "170", "320", "312", "49", "298", "196", "388", "488", "588", "188", "88", "14"],
+    ["from 1889 to 1947, what was the government type of imperial japan?", "unitary parliamentary semi-constitutional monarchy", "unitary absolute monarchy", "under military occupation", "unitary parliamentary constitutional monarchy", "unitary elective absolute monarchy", "unitary parliamentary elective semi-constitutional monarchy", "unitary parliamentary elective constitutional monarchy", "federal absolute monarchy", "federal parliamentary semi-constitutional monarchy", "federal parliamentary constitutional monarchy", "federal parliamentary elective semi-constitutional monarchy", "federal parliamentary elective constitutional monarchy", "federal elective absolute monarchy"],
+    ["out of the following, who was a prussian military commander during the battle of kolding?", "eduard von fransecky", "frederick rubeck bülow", "olaf rye", "c.f. moltke", "albert von rauch", "cäsar rüstow", "maximilian von versen", "karl von bülow"],
+    ["who was the fourth librarian of congress?", "john silva meehan", "john j. beckley", "patrick magruder", "george watterston", "john gould stephenson", "ainsworth rand spofford", "john russell young", "herbert putnam", "archibald macleish", "luther h. evans", "lawrence quincy mumford", "daniel j. boorstin", "james h. billington", "carla hayden", "john jacob astor iv", "george w. armstrong"],
+    ["how many votes did abraham lincoln receive in the new salem precint during his campaign for the illinois house of representatives in 1832?", "277", "278", "279", "280", "281", "282", "283", "284", "285", "286", "287", "288", "289", "290", "291", "292", "293", "294", "295", "296", "297", "298", "299", "300", "276", "275", "274", "273", "272", "271", "270", "269", "268", "267", "266", "265", "264", "263", "262", "261", "260", "259", "258", "257", "256", "255", "254", "253", "252", "251", "250"],
+    ["what year was iceland's parliament established", "930", "940", "950", "960", "970", "980", "990", "1000", "1010", "1020", "1030", "1040", "1050", "1060", "1070", "1080", "1090", "1100", "1110", "1120", "1130", "1140", "1150", "1160", "1170", "1180", "1190", "1200", "1210", "1220", "1230", "1240", "1250", "1260", "1270", "1280", "1290", "1300", "1310", "1320", "1330", "1340", "1350", "1360", "1370", "1380", "1390", "1400", "1410", "1420", "1430", "1440", "1450", "1460", "1470", "1480", "1490", "1500", "920", "910", "900", "890", "880", "870", "860", "850", "840", "830", "820", "810", "800", "790", "780", "770", "760", "750", "740", "730", "720", "710", "700", "690", "680", "670", "660", "650", "640", "630", "620", "610", "600", "590", "580", "570", "560", "550", "540", "530", "520", "510", "500"],
+    ["who was the english commander during the battle of morlaix?", "william de bohun, 1st earl of northampton", "thomas beaufort, duke of exeter", "john holland, 1st duke of exeter", "richard fitzalan, 3rd earl of arundel", "robert de ashton", "john beaumont, 4th baron beaumont", "guy bryan, 1st baron bryan", "simon burley", "hugh calveley", "reynold cobham, 1st baron cobham of sterborough", "john de cobham, 2nd baron cobham of kent", "philip courtenay", "walter manny, 1st baron manny", "robert morley, 2nd baron morley", "john perbroun", "william latimer, 4th baron latimer", "ralph de spigumell", "peter bard", "thomas percy, 1st earl of worchester", "edward, 2nd duke of york", "john peche", "richard pembridge", "michael de la pole, 1st earl of suffolk", "robert ufford, 1st earl of suffolk"],
+    ["who was the united states attorney general from 1933 to 1939", "homer stille cummings", "william d. mitchell", "frank murphy", "robert h. jackson", "francis biddle", "tom c. clark", "j. howard mcgrath", "james p. mcgranery", "herbert brownell jr.", "william p. rogers", "robert f. kennedy", "nicholas katzenbach", "john g. sargent", "harlan f. stone", "harry m. daugherty", "a. mitchell palmer", "thomas watt gregory", "james c. mcreynolds"],
+    ["who was the first vice governor-general of the congo free state under governor-general théophile wahis?", "francis dhanis", "camille coquilhat", "henri gondry", "herman ledeganck", "camille janssen", "émile wangermée", "alphonse van gèle", "paul costermans", "félix fuchs", "albert lantonnois van rode", "francis de winton", "eugène henry", "maurice lippens", "martin rutten", "auguste tilkens", "pierre ryckmans", "eugène jungers", "léo pétillon", "hendrik cornelis", "théophile wahis"],
+];
 const diff = {
     easy: document.getElementById("easy"),
     medium: document.getElementById("medium"),
-    hard: document.getElementById("hard")
+    hard: document.getElementById("hard"),
 };
-const select = document.getElementById("select-difficulty");
-function chooseDifficulty(d) {
-    diff[d].classList.add("expand");
+const diff_special = {
+    hyperspecific: document.getElementById("hyperspecific"),
+    all: document.getElementById("all")
+};
+function chooseDifficulty(d, special = false) {
+    const select = special ? document.getElementById("special-difficulty") : document.getElementById("select-difficulty");
+    if (special)
+        diff_special[d].classList.add("expand");
+    else
+        diff[d].classList.add("expand");
     select.classList.add("expand-wrapper");
     progressBar.style.transform = `translateY(0%)`;
     generateQuestion(d);
     setTimeout(() => {
         questionPage.classList.remove("none");
-        diff[d].classList.add("difficulty-transition");
+        if (special)
+            diff_special[d].classList.add("difficulty-transition");
+        else
+            diff[d].classList.add("difficulty-transition");
         otherDifficulties(d, "none");
         setTimeout(() => {
             select.classList.add("none");
-            diff[d].classList.remove("expand");
+            if (special)
+                diff_special[d].classList.remove("expand");
+            else
+                diff[d].classList.remove("expand");
             select.classList.remove("expand-wrapper");
-            diff[d].classList.remove("difficulty-transition");
+            if (special)
+                diff_special[d].classList.remove("difficulty-transition");
+            else
+                diff[d].classList.remove("difficulty-transition");
             otherDifficulties(d, "none", "remove");
         }, 2000);
     }, 1500);
@@ -89,6 +117,22 @@ function otherDifficulties(d, c, a = "add") {
             diff.medium.classList.remove(c);
         }
     }
+    if (d === "hyperspecific") {
+        if (a === "add") {
+            diff_special.all.classList.add(c);
+        }
+        else if (a === "remove") {
+            diff_special.all.classList.remove(c);
+        }
+    }
+    else if (d === "all") {
+        if (a === "add") {
+            diff_special.hyperspecific.classList.add(c);
+        }
+        else if (a === "remove") {
+            diff_special.hyperspecific.classList.remove(c);
+        }
+    }
 }
 diff.easy.onclick = () => {
     if (![...diff.easy.classList].includes("expand")) {
@@ -121,6 +165,7 @@ function generateQuestion(difficulty) {
     // get all questions in selected difficulty
     let data;
     switch (difficulty) {
+        // regular difficulties
         case "easy":
             data = [...easyQuestions];
             break;
@@ -129,6 +174,10 @@ function generateQuestion(difficulty) {
             break;
         case "hard":
             data = [...hardQuestions];
+            break;
+        // special difficulties
+        case "hyperspecific":
+            data = [...hyperspecificQuestions];
             break;
         default:
             data = [...easyQuestions, ...mediumQuestions, ...hardQuestions];
@@ -185,7 +234,7 @@ function generateQuestion(difficulty) {
                     b.setAttribute("click", "0");
                 setTimeout(() => {
                     // pick a random transition
-                    const r = randomInRange(1, 5);
+                    const r = randomInRange(1, 6);
                     const l = [document.querySelector(".question"), ...buttons];
                     questionPage.classList.add(`transition${r}`); // choose transition
                     // add score + update progress bar if you chose the correct answer
@@ -223,8 +272,8 @@ function generateQuestion(difficulty) {
                                         }, 1000);
                                     }, 1000);
                                 }, 2500);
-                            }, 1000);
-                        }, r !== 4 ? 2000 : 930);
+                            }, 1000); // @ts-ignore
+                        }, r !== 4 ? 2000 : 923);
                     }
                     else {
                         /*
@@ -305,8 +354,40 @@ document.head.insertAdjacentHTML("beforeend", `<style>
     animation: ${Math.random() + 1}s transition2 ease-in-out forwards;
 }
 </style>`);
+let clickCount = 0;
+const title = document.querySelector(".title");
+const special = document.getElementById("special-difficulty");
+title.onclick = () => {
+    clickCount += 1;
+    setTimeout(() => {
+        clickCount = 0;
+    }, 5000); // 5 seconds
+    if (clickCount === 5) {
+        home.classList.add("animation");
+        setTimeout(() => {
+            special.classList.remove("none");
+        }, 1000);
+        setTimeout(() => {
+            home.classList.remove("animation");
+            home.classList.add("none");
+        }, 2000);
+    }
+};
+diff_special.hyperspecific.onclick = () => {
+    if (![...diff_special.hyperspecific.classList].includes("expand")) {
+        home.classList.add("none");
+        return chooseDifficulty("hyperspecific", true);
+    }
+};
+diff_special.all.onclick = () => {
+    if (![...diff_special.all.classList].includes("expand")) {
+        home.classList.add("none");
+        return chooseDifficulty("all", true);
+    }
+};
 const home = document.getElementById("home");
 const homePlay = document.getElementById("home-play");
+const select = document.getElementById("select-difficulty");
 homePlay.onclick = () => {
     home.classList.add("animation");
     setTimeout(() => {
